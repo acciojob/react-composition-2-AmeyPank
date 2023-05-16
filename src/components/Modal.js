d82372f5fa5cf5f638e17a514bb2e343286e080d
AmeyPank <pankantiameya@gmail.com>
2023-05-16 12:49:27 +0530

    outside of modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 const Modal = ({ show, onClose, children }) => {
+  const modalRef = useRef(null);
+
+  const handleCloseModal = (event) => {
+    if (event.target === modalRef.current) {
+      onClose();
+    }
+  };
+
   if (!show) {
     return null;
   }
 
   return (
-    <div className="modal-overlay">
+    <div className="modal-overlay" onClick={handleCloseModal} ref={modalRef}>
       <div className="modal-dialog">
         <button className="modal-close" onClick={onClose}>
           Close
