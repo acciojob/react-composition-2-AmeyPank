84a0d087ad92a60aef954ae9fc55dcdf1f67e507
AmeyPank <pankantiameya@gmail.com>
2023-05-23 01:29:28 +0530

    /* style.css */ .modal-overlay {   position: fixed;   top: 0;   left: 0;   width: 100%;   height: 100%;   background-color: rgba(0, 0, 0, 0.5);   display: flex;   align-items: center;   justify-content: center; }
    .modal-dialog {
      background-color: #fff;
      padding: 20px;
      border-radius: 5px;
      box-shadow: 0 2px 4px rgba(0, 0, 0, 0.3);
    }
    
    .modal-content {
      text-align: center;
    }
    
    .modal-close {
      position: absolute;
      top: 10px;
      right: 10px;
      background: none;
      border: none;
      font-size: 16px;
      cursor: pointer;
    }

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Modal from './Modal';
+import "../styles/App.css"
 
 const App = () => {
   const [showModal, setShowModal] = useState(false);
@@ -13,8 +14,11 @@ const App = () => {
       <button onClick={() => setShowModal(true)}>Open Modal</button>
       <Modal show={showModal} onClose={handleCloseModal}>
         {/* Content for the modal */}
+        <div className='moal'>
         <h2>Modal Title</h2>
-        <p className='modal-p'>This is the modal content.</p>
+        <p>This is the modal content.</p>
+        </div>
+
       </Modal>
     </div>
   );
